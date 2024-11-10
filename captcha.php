7794617b387e329b43f450357f5aa37be6a07ab8
hallexz <yevtefeevah@gmail.com>
2024-11-10 14:21:43 +0700

    add captcha.php

---
+++ captcha.php
@@ -0,0 +1,25 @@
+<?php
+
+define('SMARTCAPTCHA_SERVER_KEY');
+
+function check_captcha($token) {
+    $ch = curl_init();
+    $args = http_build_query([
+        "secret" => SMARTCAPTCHA_SERVER_KEY,
+        "token" => $token,
+        "ip" => $_SERVER['REMOTE_ADDR'], // Нужно передать IP пользователя.
+    ]);
+    curl_setopt($ch, CURLOPT_URL, "https://smartcaptcha.yandexcloud.net/validate?$args");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Увеличиваем таймаут
+    $server_output = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpcode !== 200) {
+        echo "Ошибка при обращении к серверу капчи: код=$httpcode; сообщение=$server_output\n";
+        return false;
+    }
+    $resp = json_decode($server_output);
+    return $resp->status === "ok";
+}
